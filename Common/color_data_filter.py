ffccc42cba090dae9eddfc7f8492bde2e634c053
wmm98 <792545884@qq.com>
2024-06-24 16:30:09 +0800

    update

--- Common/color_data_filter.py
+++ Common/color_data_filter.py
@@ -33,7 +33,9 @@ class CSVTestData(Interface):
         balance_err_array = data[row_num: row_num + 6]
         # 按顺序返回19-24的数据['0.166', '0.104', '0.098', '0.129', '0.192', '0.405']
         result = [line[-1] for line in balance_err_array]
-        return {"19": float(result[0]), "20": float(result[1]), "21": float(result[2]), "22": float(result[3]), "23": float(result[4]), "24": float(result[5])}
+        return {conf.nineteen: float(result[0]), conf.twenty: float(result[1]), conf.twenty_one: float(result[2]),
+                conf.twenty_two: float(result[3]), conf.twenty_three: float(result[4]),
+                conf.twenty_four: float(result[5])}
 
     def get_rgby_noise_data(self, data):
         row_num = 0
@@ -50,19 +52,81 @@ class CSVTestData(Interface):
         float_err_array = [[float(j) for j in i] for i in err_array]
         # 返回平均数，即为RGBY,取小数点后两位 "{:.2f}".format()
         RGBY = [float("{:.2f}".format((sum(e_a) / len(e_a)))) for e_a in float_err_array]
-        return {"R": RGBY[0], "G": RGBY[1], "B": RGBY[2], "Y": RGBY[3]}
+        return {conf.R: RGBY[0], conf.G: RGBY[1], conf.B: RGBY[2], conf.Y: RGBY[3]}
 
     def get_snr_data(self, data):
         for row in data:
             line = [self.remove_space(i) for i in row]
             if self.remove_space(conf.snr_bw) in line[0]:
-                return {"R": float(line[1]), "G": float(line[2]), "B": float(line[3]), "Y": float(line[4])}
+                return {conf.R: float(line[1]), conf.G: float(line[2]), conf.B: float(line[3]), conf.Y: float(line[4])}
+
+    def get_color_accuracy_data(self):
+        accuracy_data = {}
+        flag = 0
+        for row in data:
+            line = [self.remove_space(i) for i in row]
+            if self.remove_space(conf.C1_key) in line:
+                flag += 1
+                accuracy_data[conf.C1] = float(line[1])
+                continue
+            if self.remove_space(conf.C2_key) in line:
+                flag += 1
+                accuracy_data[conf.C2] = float(line[1])
+                continue
+            if self.remove_space(conf.E1_key) in line:
+                flag += 1
+                accuracy_data[conf.E1] = float(line[1])
+                continue
+            if self.remove_space(conf.Sat_key) in line:
+                flag += 1
+                accuracy_data[conf.Sat] = float(line[1])
+                continue
+            if flag == 4:
+                break
+        return accuracy_data
+
+    def get_HJ_contrast_and_readable_data(self, data):
+        row_num = 0
+        for row in data:
+            row_num += 1
+            if self.remove_space(conf.hj_relate_key) in [self.remove_space(i) for i in row]:
+                break
+        # 取第二列数据
+        relate_data = np.array(data)[row_num: row_num + 20, 1]
+        zone_data = [float(i) for i in relate_data.tolist()]
+
+        # 前后相减， 获取可读灰阶数
+        zone_length = len(zone_data)
+        readable_relate = [float("{:.1f}".format(zone_data[r] - zone_data[r + 1])) if r < (zone_length - 1) else float(
+            "{:.1f}".format(zone_data[-1])) for r in range(zone_length)]
+        readable_num = len([i for i in readable_relate if i >= 8])
+
+        # 获取对比度
+        contrast = "{:.2f}".format(((zone_data[0] - zone_data[-1]) / 255))
+        return {conf.hj_readable: readable_num, conf.hj_contrast: contrast}
+
+    def get_hj_dynamic_range_data(self, data):
+        for row in data:
+            if self.remove_space(conf.dynamic_range_key) in [self.remove_space(i) for i in row]:
+                return float(row[2])
+
+    def get_hj_relate_data(self, t_data):
+        contrast_readable = self.get_HJ_contrast_and_readable_data(t_data)
+        return {conf.hj_dynamic_range: self.get_hj_dynamic_range_data(t_data),
+                conf.hj_readable: contrast_readable[conf.hj_readable],
+                conf.hj_contrast: contrast_readable[conf.hj_contrast]}
 
 
 if __name__ == '__main__':
     cvs_data = CSVTestData(conf.f_data_path)
     data = cvs_data.read_csv_to_matrix()
     # print(data)
-    # print(cvs_data.get_white_balance_err_data(data))
+    print(cvs_data.get_white_balance_err_data(data))
     print(cvs_data.get_rgby_noise_data(data))
-    # print(cvs_data.get_snr_data(data))
+    print(cvs_data.get_snr_data(data))
+    print(cvs_data.get_color_accuracy_data())
+    print("***************************")
+    cvs_data = CSVTestData(conf.hj_data_path)
+    data = cvs_data.read_csv_to_matrix()
+    hj_relate_data = cvs_data.get_hj_relate_data(data)
+    print(hj_relate_data)
--- Conf/config.py
+++ Conf/config.py
@@ -20,4 +20,33 @@ class Config:
     b_noise = "B-noise%"
     snr_bw = "SNR_BW"
 
+    # 色彩偏差关键字
+    Sat_key = "Mean camera chroma %"
+    C1_key = "Mean  Delta-C sat corr"
+    C2_key = "Mean  Delta-C uncorr"
+    E1_key = "Mean  Delta-E*ab uncorr"
 
+    # 列
+    R = "R"
+    G = "G"
+    B = "B"
+    Y = "Y"
+
+    nineteen = "19"
+    twenty = "20"
+    twenty_one = "21"
+    twenty_two = "22"
+    twenty_three = "23"
+    twenty_four = "24"
+
+    C1 = "C1"
+    C2 = "C2"
+    E1 = "E1"
+    Sat = "Sat"
+
+    dynamic_range_key = "Total"
+    hj_relate_key = "Pixel/255"
+
+    hj_dynamic_range = "dynamic_range"
+    hj_contrast = "contrast"
+    hj_readable = "readable"
