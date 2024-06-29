26fd6b3f933f186e561fad77d7363027f52357cb
wmm98 <792545884@qq.com>
2024-06-29 11:20:53 +0800

    update

---
+++ UI/Imatest_Run -origin.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import sys
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt, QTimer, QRunnable, QThreadPool
+from Tree_Widget import Ui_MainWindow
+import yaml
+import os
+import shutil
+import threading
+
+
+class tree(QtWidgets.QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super(tree, self).__init__()
+        self.setupUi(self)
+        self.intiui()
+        self.camera_param = 0
+        self.final_report_name = ""
+        self.err_flag = 0
+
+    def intiui(self):
+        self.standard_group.buttonClicked[int].connect(self.on_check_box_standard_clicked)
+        self.group.buttonClicked[int].connect(self.on_check_box_clicked)
+        self.F_data_upload_button.clicked.connect(self.upload_F_csv_file)
+        self.D65_data_upload_button.clicked.connect(self.upload_D65_csv_file)
+        self.TL84_data_upload_button.clicked.connect(self.upload_TL84_csv_file)
+        self.CWF_data_upload_button.clicked.connect(self.upload_CWF_csv_file)
+        self.HJ_data_upload_button.clicked.connect(self.upload_HJ_csv_file)
+
+        self.submit_button.clicked.connect(self.handle_submit)
+
+    def get_message_box(self, text):
+        QMessageBox.warning(self, "错误提示", text)
+
+    def handle_submit(self):
+        # 文本框非空检查
+        if not self.is_quality_device.isChecked() and not self.is_standard_device.isChecked():
+            self.get_message_box("请勾选设备为精品或者标准!!!")
+            return
+        if not self.is_800_camera.isChecked() and not self.is_500_camera.isChecked() and not self.is_200_camera.isChecked() \
+                and not self.is_1600_camera.isChecked() and not self.is_1300_camera.isChecked():
+            self.get_message_box("请勾选其中一个摄像头参数!!!")
+            return
+        if len(self.camera_product_edit.text()) == 0:
+            # 显示错误消息框
+            self.get_message_box("摄像头厂家不能为空,请输入!!!")
+            return
+        if len(self.project_edit.text()) == 0:
+            self.get_message_box("项目名称不能为空, 请输入!!!")
+            return
+        F_path_length = len(self.F_file_path.text())
+        D65_path_length = len(self.D65_file_path.text())
+        TL84_path_length = len(self.TL84_file_path.text())
+        CWF_path_length = len(self.CWF_file_path.text())
+        HJ_path_length = len(self.HJ_file_path.text())
+        if F_path_length == 0 and D65_path_length == 0 and TL84_path_length == 0 and CWF_path_length == 0 and HJ_path_length == 0:
+            self.get_message_box("请上传测试的csv!!!")
+            return
+
+        # # 检设备名字，检查check box 属性
+        self.data["CameraData"]["pixels"] = self.camera_param
+        self.data["CameraData"]["project_name"] = self.project_edit.text()
+        self.data["CameraData"]["camera_product"] = self.camera_product_edit.text()
+        self.data["CameraData"]["is_f_test"] = True if F_path_length != 0 else False
+        self.data["CameraData"]["is_d65_test"] = True if D65_path_length != 0 else False
+        self.data["CameraData"]["is_tl84_test"] = True if TL84_path_length != 0 else False
+        self.data["CameraData"]["is_cwf_test"] = True if CWF_path_length != 0 else False
+        self.data["CameraData"]["is_hj_test"] = True if HJ_path_length != 0 else False
+
+        # 拷贝csv文件到相应的目录
+        if HJ_path_length != 0:
+            HJ_file_path = self.HJ_file_path.text()
+            self.deal_csv_file("HJ", HJ_file_path)
+
+        if F_path_length != 0:
+            f_file_path = self.F_file_path.text()
+            self.deal_csv_file("F", f_file_path)
+
+        if D65_path_length != 0:
+            D65_file_path = self.D65_file_path.text()
+            self.deal_csv_file("D65", D65_file_path)
+
+        if TL84_path_length != 0:
+            TL84_file_path = self.TL84_file_path.text()
+            self.deal_csv_file("TL84", TL84_file_path)
+
+        if CWF_path_length != 0:
+            CWF_file_path = self.CWF_file_path.text()
+            self.deal_csv_file("CWF", CWF_file_path)
+
+        # 检测报告的生成
+        self.final_report_name = "%s-%s-%d万摄像头-指标测试报告.xlsx" % (self.data["CameraData"]["camera_product"],
+                                                                        self.data["CameraData"]["project_name"],
+                                                                        int(self.data["CameraData"]["pixels"]))
+
+        # 已存在的报告,生成新的
+        if self.path_is_existed(os.path.join(self.project_path, self.final_report_name)):
+            self.err_flag += 1
+            self.final_report_name = "%s-%s-%d万摄像头-指标测试报告(%d).xlsx" % (
+                self.data["CameraData"]["camera_product"],
+                self.data["CameraData"]["project_name"],
+                int(self.data["CameraData"]["pixels"]),
+                self.err_flag)
+
+        self.data["CameraData"]["report_err_flag"] = self.err_flag
+        self.data["CameraData"]["report_file_name"] = self.final_report_name
+        # 保存修改后的内容回 YAML 文件
+        with open(self.yaml_file_path, 'w') as file:
+            yaml.safe_dump(self.data, file)
+
+        # 显示报告正在生成中
+        self.tips.setText("正在生成报告,请等待.....")
+        # 单独线程运行,避免阻塞主线程和 PyQt5 的事件
+        thread = threading.Thread(target=self.run_process)
+        thread.start()
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.check_report)
+
+        self.check_interval = 1000  # 定时器间隔，单位毫秒
+        self.timeout_limit = 60 * 1000  # 超时限制，单位毫秒, 10秒超时
+        self.elapsed_time = 0  # 已经过的时间
+
+        self.timer.start(self.check_interval)  # 启动定时器
+
+    def check_report(self):
+        path = os.path.join(self.project_path, self.final_report_name)  # 要检查的路径
+        if os.path.exists(path):
+            self.tips.setText("报告已经生成:  %s" % self.final_report_name)
+            self.timer.stop()  # 如果报告存在，停止定时器
+        else:
+            self.elapsed_time += self.check_interval
+            if self.elapsed_time >= self.timeout_limit:
+                self.tips.setText("生成报告失败,请再次生成")
+                self.timer.stop()  # 如果超时，停止定时器
+
+    def closeEvent(self, event):
+        self.timer.stop()  # 在窗口关闭时停止定时器
+        event.accept()
+
+    def run_process(self):
+        subprocess.run([os.path.join(self.project_path, "Run", "bat_run.bat")])
+
+    def check_file_extension_name(self, file_name, light):
+        if ".csv" != os.path.splitext(file_name)[1].strip():
+            self.get_message_box("%s光数据请上传csv格式的数据!!!" % light)
+            return False
+        return True
+
+    def deal_csv_file(self, light, file_path):
+        csv_name = light + os.path.splitext(file_path)[1]
+        test_data_path = os.path.join(self.project_path, "TestData")
+        des_folder = os.path.join(test_data_path, light)
+        des_file = os.path.join(des_folder, csv_name)
+        file_copied_path = os.path.join(des_folder, os.path.basename(file_path))
+        self.remove_file(des_file)
+        self.remove_file(des_file)
+        self.copy_file(file_path, des_folder)
+        if not self.path_is_existed(des_file):
+            self.copy_file(file_path, des_folder)
+        self.rename_file(file_copied_path, des_file)
+
+    def on_check_box_clicked(self, id):
+        # 处理复选框点击事件
+        if id == 1:
+            self.camera_param = 800
+        elif id == 2:
+            self.camera_param = 500
+        elif id == 3:
+            self.camera_param = 200
+        elif id == 4:
+            self.camera_param = 1300
+        elif id == 5:
+            self.camera_param = 1600
+
+    def on_check_box_standard_clicked(self, id):
+        if id == 1:
+            self.data["CameraData"]["standard"] = False
+        else:
+            self.data["CameraData"]["standard"] = True
+
+    def copy_file(self, origin, des):
+        shutil.copy(origin, des)
+
+    def rename_file(self, origin, des):
+        shutil.move(origin, des)
+
+    def remove_file(self, path):
+        if os.path.isfile(path):
+            os.remove(path)
+
+    def path_is_existed(self, path):
+        if os.path.exists(path):
+            return True
+        else:
+            return False
+
+    def upload_F_csv_file(self):
+        # 打开文件选择对话框
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", "",
+                                                             "CSV files (*.csv)",
+                                                             options=QtWidgets.QFileDialog.Options())
+        if file_name:
+            self.F_file_path.setText(file_name)
+
+    def upload_D65_csv_file(self):
+        # 打开文件选择对话框
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", "",
+                                                             "CSV files (*.csv)",
+                                                             options=QtWidgets.QFileDialog.Options())
+        if file_name:
+            self.D65_file_path.setText(file_name)
+
+    def upload_TL84_csv_file(self):
+        # 打开文件选择对话框
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", "",
+                                                             "CSV files (*.csv)",
+                                                             options=QtWidgets.QFileDialog.Options())
+        if file_name:
+            self.TL84_file_path.setText(file_name)
+
+    def upload_CWF_csv_file(self):
+        # 打开文件选择对话框
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", "",
+                                                             "CSV files (*.csv)",
+                                                             options=QtWidgets.QFileDialog.Options())
+        if file_name:
+            self.CWF_file_path.setText(file_name)
+
+    def upload_Mix_csv_file(self):
+        # 打开文件选择对话框
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", "",
+                                                             "CSV files (*.csv)",
+                                                             options=QtWidgets.QFileDialog.Options())
+        if file_name:
+            self.Mix_file_path.setText(file_name)
+
+    def upload_HJ_csv_file(self):
+        # 打开文件选择对话框
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "选择文件", "",
+                                                             "CSV files (*.csv)",
+                                                             options=QtWidgets.QFileDialog.Options())
+        if file_name:
+            self.HJ_file_path.setText(file_name)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myshow = tree()
+    myshow.show()
+    sys.exit(app.exec_())
---
+++ UI/Tree_Widget - origin.py
@@ -0,0 +1,151 @@
+import os
+import yaml
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QHBoxLayout, QCheckBox, QLineEdit, QCompleter, QComboBox, QButtonGroup
+
+
+class Ui_MainWindow(object):
+    options = QtWidgets.QFileDialog.Options()
+    options |= QtWidgets.QFileDialog.ReadOnly
+    project_path = path_dir = str(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+    print(project_path)
+
+    yaml_file_path = os.path.join(project_path, "Conf", "test_data.yaml")
+    # 加载 YAML 文件
+    with open(yaml_file_path, 'r', encoding="utf-8") as file:
+        data = yaml.safe_load(file)
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(700, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.device_info = QtWidgets.QLabel("摄像头信息：")
+        self.verticalLayout.addWidget(self.device_info)
+
+        # 将标签添加到水平布局中
+        # 添加checkbox
+        standard_layout = QHBoxLayout()
+        self.is_standard_device = QCheckBox("标准")
+        self.is_quality_device = QCheckBox("精品")
+        standard_layout.addWidget(self.is_standard_device)
+        standard_layout.addWidget(self.is_quality_device)
+        standard_layout.addStretch(1)
+        self.standard_group = QButtonGroup()
+        self.standard_group.addButton(self.is_standard_device, id=1)
+        self.standard_group.addButton(self.is_quality_device, id=2)
+        # 设置按钮组中只能有一个选中
+        self.standard_group.setExclusive(True)
+        self.verticalLayout.addLayout(standard_layout)
+
+        layout = QHBoxLayout()
+        self.is_800_camera = QCheckBox("800万")
+        self.is_500_camera = QCheckBox("500万")
+        self.is_200_camera = QCheckBox("200万")
+        self.is_1300_camera = QCheckBox("1300万")
+        self.is_1600_camera = QCheckBox("1600万")
+
+        layout.addWidget(self.is_800_camera)
+        layout.addWidget(self.is_500_camera)
+        layout.addWidget(self.is_200_camera)
+        layout.addWidget(self.is_1300_camera)
+        layout.addWidget(self.is_1600_camera)
+        # 添加一个拉伸因子以将水平布局放在窗口底部
+        layout.addStretch(1)
+        # 将水平布局放入垂直布局
+
+        # 创建按钮组并将复选框添加到按钮组中
+        self.group = QButtonGroup()
+        self.group.addButton(self.is_800_camera, id=1)
+        self.group.addButton(self.is_500_camera, id=2)
+        self.group.addButton(self.is_200_camera, id=3)
+        self.group.addButton(self.is_1300_camera, id=4)
+        self.group.addButton(self.is_1600_camera, id=5)
+
+        # 设置按钮组中只能有一个选中
+        self.group.setExclusive(True)
+        self.verticalLayout.addLayout(layout)
+
+        layout_product = QHBoxLayout()
+        self.camera_product_name = QtWidgets.QLabel("摄像头厂家：")
+        self.camera_product_edit = QtWidgets.QLineEdit()
+        self.project_name = QtWidgets.QLabel("项目名称：")
+        self.project_edit = QtWidgets.QLineEdit()
+        layout_product.addWidget(self.camera_product_name)
+        layout_product.addWidget(self.camera_product_edit)
+        layout_product.addWidget(self.project_name)
+        layout_product.addWidget(self.project_edit)
+        self.verticalLayout.addLayout(layout_product)
+
+        # color check 数据上传
+        self.color_HJ_info = QtWidgets.QLabel("上传灰阶csv：")
+        self.verticalLayout.addWidget(self.color_HJ_info)
+        layout_HJ_light = QHBoxLayout()
+        self.HJ_file_path = QtWidgets.QLineEdit()
+        layout_HJ_light.addWidget(self.HJ_file_path)
+        self.HJ_data_upload_button = QtWidgets.QPushButton("点击上传")
+        layout_HJ_light.addWidget(self.HJ_data_upload_button)
+        self.verticalLayout.addLayout(layout_HJ_light)
+
+        self.color_F_info = QtWidgets.QLabel("上传F光csv：")
+        self.verticalLayout.addWidget(self.color_F_info)
+        layout_F_light = QHBoxLayout()
+        self.F_file_path = QtWidgets.QLineEdit()
+        layout_F_light.addWidget(self.F_file_path)
+        self.F_data_upload_button = QtWidgets.QPushButton("点击上传")
+        layout_F_light.addWidget(self.F_data_upload_button)
+        self.verticalLayout.addLayout(layout_F_light)
+
+        self.color_D65_info = QtWidgets.QLabel("上传D65光csv：")
+        self.verticalLayout.addWidget(self.color_D65_info)
+        layout_D65_light = QHBoxLayout()
+        self.D65_file_path = QtWidgets.QLineEdit()
+        layout_D65_light.addWidget(self.D65_file_path)
+        self.D65_data_upload_button = QtWidgets.QPushButton("点击上传")
+        layout_D65_light.addWidget(self.D65_data_upload_button)
+        self.verticalLayout.addLayout(layout_D65_light)
+
+        self.color_CWF_info = QtWidgets.QLabel("上传CWF光csv：")
+        self.verticalLayout.addWidget(self.color_CWF_info)
+        layout_CWF_light = QHBoxLayout()
+        self.CWF_file_path = QtWidgets.QLineEdit()
+        layout_CWF_light.addWidget(self.CWF_file_path)
+        self.CWF_data_upload_button = QtWidgets.QPushButton("点击上传")
+        layout_CWF_light.addWidget(self.CWF_data_upload_button)
+        self.verticalLayout.addLayout(layout_CWF_light)
+
+        self.color_TL84_info = QtWidgets.QLabel("上传TL84光csv：")
+        self.verticalLayout.addWidget(self.color_TL84_info)
+        layout_TL84_light = QHBoxLayout()
+        self.TL84_file_path = QtWidgets.QLineEdit()
+        layout_TL84_light.addWidget(self.TL84_file_path)
+        self.TL84_data_upload_button = QtWidgets.QPushButton("点击上传")
+        layout_TL84_light.addWidget(self.TL84_data_upload_button)
+        self.verticalLayout.addLayout(layout_TL84_light)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 0, 0))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        # 提交按钮
+        self.submit_button = QtWidgets.QPushButton("开始生成报告")
+        self.verticalLayout.addWidget(self.submit_button)
+
+        self.tips = QtWidgets.QLabel()
+        self.tips.setStyleSheet("color: red;")
+        self.tips.setText("未开始生成报告...")
+        # self.tips.setVisible(False)
+        self.verticalLayout.addWidget(self.tips)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
